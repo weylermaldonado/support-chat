f5fe1b2417e7bc6ead2b3c55d826378aa4028360
WeylerMaldonado <weyler.maldonado@gmail.com>
2018-02-23 14:56:33 -0600

    Diseño de base de datos agregada, vistas y controlador de salas de chat agregadas

---
+++ app/controllers/chat_room_controller.rb
@@ -0,0 +1,27 @@
+class ChatRoomsController < ApplicationController
+    def index
+        @chat_rooms = ChatRoom.all 
+    end
+    
+    def new
+        @chat_room = ChatRoom.new
+    end
+
+    def create
+        @chat_room = current_user.chat_rooms.build(chat_room_params)
+        if @chat_room.save
+          flash[:success] = "Sala de chat creada"
+          redirect_to @chat_rooms_path
+        else
+          #flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+
+    def chat_room_params
+        params.require(:chat_room).permit(:title)
+    end
+    
+    
+    
+end
