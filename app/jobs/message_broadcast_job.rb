5f2eb8dab6b42030bba277ba36da70ca3116584d
WeylerMaldonado <weyler.maldonado@gmail.com>
2018-03-05 23:27:14 -0600

    algo

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -1,7 +1,7 @@
 class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
-  def perform(message)
+  def perform(message)  
     ActionCable.server.broadcast "chat_rooms_#{message.chat_room.id}_channel",
                                  message: render_message(message)
   end
